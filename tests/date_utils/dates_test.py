9f108bb9518882df30526f684cfd58dad82e2904
Jason Green <justjasongreen@gmail.com>
2016-07-24 10:17:45 +1000

    Implement tests.date_utils.dates_test.test_forward

---
+++ tests/date_utils/dates_test.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from predictive_punter import date_utils
+
+
+def test_forward():
+    """The dates iterator should generate the expected dates when date_from > date_to"""
+
+    expected_dates = [
+        datetime(2016, 2, 1),
+        datetime(2016, 2, 2),
+        datetime(2016, 2, 3)
+    ]
+
+    actual_dates = [date for date in date_utils.dates(datetime(2016, 2, 1), datetime(2016, 2, 3))]
+
+    assert actual_dates == expected_dates
