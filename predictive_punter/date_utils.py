03a2c7cce2732ed1573bc780bfe900f985d8568f
Jason Green <justjasongreen@gmail.com>
2016-07-24 10:23:32 +1000

    Implement tests.date_utils.dates_test.test_reverse

--- predictive_punter/date_utils.py
+++ predictive_punter/date_utils.py
@@ -4,7 +4,11 @@ from datetime import timedelta
 def dates(date_from, date_to):
     """Iterate over all dates from date_from to date_to (inclusive)"""
 
+    time_delta = timedelta(days=1)
+    if date_from > date_to:
+        time_delta *= -1
+
     next_date = date_from
-    while next_date <= date_to:
+    while (date_from <= date_to and next_date <= date_to) or (date_from > date_to and next_date >= date_to):
         yield next_date
-        next_date += timedelta(days=1)
+        next_date += time_delta
--- tests/date_utils/dates_test.py
+++ tests/date_utils/dates_test.py
@@ -4,7 +4,7 @@ from predictive_punter import date_utils
 
 
 def test_forward():
-    """The dates iterator should generate the expected dates when date_from > date_to"""
+    """The dates iterator should generate the expected dates when date_from < date_to"""
 
     expected_dates = [
         datetime(2016, 2, 1),
@@ -13,5 +13,19 @@ def test_forward():
     ]
 
     actual_dates = [date for date in date_utils.dates(datetime(2016, 2, 1), datetime(2016, 2, 3))]
+
+    assert actual_dates == expected_dates
+
+
+def test_reverse():
+    """The dates iterator should generate the expected dates when date_from > date_to"""
+
+    expected_dates = [
+        datetime(2016, 2, 3),
+        datetime(2016, 2, 2),
+        datetime(2016, 2, 1)
+    ]
+
+    actual_dates = [date for date in date_utils.dates(datetime(2016, 2, 3), datetime(2016, 2, 1))]
 
     assert actual_dates == expected_dates
