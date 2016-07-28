73a14449a0211858bf7909269624f56bb0663def
Jason Green <justjasongreen@gmail.com>
2016-07-28 13:58:00 +1000

    Implement tests.race_test.test_get_winning_combinations

--- predictive_punter/race.py
+++ predictive_punter/race.py
@@ -11,3 +11,45 @@ def active_runners(self):
     return self.get_cached_property('active_runners', generate_active_runners)
 
 racing_data.Race.active_runners = active_runners
+
+
+def get_winning_combinations(self, places):
+    """Return a list of tuples of Runners representing all winning combinations for the specified number of places"""
+
+    def get_combinations(results):
+
+        if len(results) > 0:
+
+            combinations = []
+
+            next_combinations = get_combinations(results[1:])
+            for item in results[0]:
+                if next_combinations is None:
+                    combinations.append([item])
+                else:
+                    for next_combination in next_combinations:
+                        combinations.append([item] + next_combination)
+
+            return combinations
+
+    results = []
+    for count in range(places):
+        results.append([])
+
+    for runner in self.active_runners:
+        if runner.result is not None and runner.result <= len(results):
+            results[runner.result - 1].append(runner)
+
+    combinations = get_combinations(results)
+    dupes = []
+    for index in range(len(combinations)):
+        for item in combinations[index]:
+            if len([combo_item for combo_item in combinations[index] if combo_item == item]) > 1:
+                dupes.append(index)
+                break
+    for index in sorted(dupes, reverse=True):
+        combinations.removeAt(index)
+
+    return [tuple(combination) for combination in combinations]
+
+racing_data.Race.get_winning_combinations = get_winning_combinations
--- tests/race_test.py
+++ tests/race_test.py
@@ -2,3 +2,21 @@ def test_active_runners(race):
     """The active_runners property should return a list containing all non-scratched runners in the race"""
 
     assert race.active_runners == [runner for runner in race.runners if runner['is_scratched'] is False]
+
+
+def test_get_winning_combinations(race):
+    """The get_winning_combinations method should return a list of tuples of Runners representing all winning combinations for the specified number of places"""
+
+    results = [
+        None,
+        None,
+        None,
+        None
+    ]
+    for runner in race.active_runners:
+        if runner.result <= len(results):
+            results[runner.result - 1] = runner
+
+    for places in range(1, 5):
+        winning_combinations = race.get_winning_combinations(places)
+        assert winning_combinations[0] == tuple([results[index] for index in range(places)])
