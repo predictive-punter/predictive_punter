3bfe73a5f7b6199c87eb030b2bd03db700fb1113
Jason Green <justjasongreen@gmail.com>
2016-08-04 21:08:23 +1000

    Seek minimum dividend instead of maximum ROI

--- predictive_punter/race.py
+++ predictive_punter/race.py
@@ -156,9 +156,9 @@ def best_predictions(self):
                             average_win_value = sum(win_values) / len(win_values)
                             strike_rate = len(win_values) / len(all_values)
                             roi = average_win_value * strike_rate
+                            minimum_dividend = 1.0 / strike_rate
 
-                            if roi > 1.0 and (best_predictions[bet_type] is None or roi > best_predictions[bet_type][1]):
-                                minimum_dividend = 1.0 / strike_rate
+                            if roi > 1.0 and (best_predictions[bet_type] is None or minimum_dividend < best_predictions[bet_type][2]):
                                 best_predictions[bet_type] = prediction, roi, minimum_dividend
 
                         if bet_type == 'win' and sum(all_values) > 0:
